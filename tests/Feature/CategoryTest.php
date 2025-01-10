76e007b5603e94dd5f763c370e6706021431b547
ahnafi <devsulthon@gmail.com>
2025-01-10 22:53:56 +0700

    learn : insert many

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -5,10 +5,18 @@ namespace Tests\Feature;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+
+    function setUp(): void
+    {
+        parent::setUp();
+        DB::delete("DELETE FROM categories");
+    }
+
     function testInsert()
     {
         $category = new Category();
@@ -19,5 +27,26 @@ class CategoryTest extends TestCase
         $result = $category->save();
 
         self::assertTrue($result);
+    }
+
+    function testInsertManyCategories()
+    {
+        $categories = [];
+
+        for ($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "name $i",
+            ];
+        }
+
+        $result = Category::query()->insert($categories);
+//        $result = Category::insert(collect($categories));
+        self::assertTrue($result);
+
+        $total = Category::query()->count();
+//        $total = Category::count();
+        self::assertEquals(10, $total);
+
     }
 }
