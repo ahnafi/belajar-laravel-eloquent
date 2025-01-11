cc4437316f41942441c0cdb68f5e6f73cb2ed095
ahnafi <devsulthon@gmail.com>
2025-01-11 23:30:13 +0700

    learn : UUID (trait uuid selain primary key )

---
+++ tests/Feature/uuidTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\voucher;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class uuidTest extends TestCase
+{
+    function setUp(): void
+    {
+        parent::setUp();
+        DB::delete("delete from vouchers");
+    }
+
+    function testCreateVoucher()
+    {
+        $voucher = new voucher();
+        $voucher->name = "sample voucher ";
+        $voucher->voucher_code = "12231231231";
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+
+    }
+
+    function testCreateVoucherUUID()
+    {
+        $voucher = new voucher();
+        $voucher->name = "sample voucher ";
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+        self::assertNotNull($voucher->voucher_code);
+
+    }
+}
